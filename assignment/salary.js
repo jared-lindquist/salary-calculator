4c7d0c8152743fb35e20e15eadae0cee06abf0ea
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-10-28 19:19:37 -0500

    edits to html/css styling

--- assignment/salary.js
+++ assignment/salary.js
@@ -1,12 +1,17 @@
 $(document).ready(onReady);
 
+//initialize global variables to be used in functions
 let employees = [];
 let arrayId = 0;
+let totalAnnualSal = 0;
+let monthlySal = 0;
+
 
 function onReady() {
     console.log('ready');
-//reference the onReady function in the event-state-render-starter
+    //click listener for submit button
     $('#submitBtn').on('click', addEmployee);
+    // click listener for removeEmployee button
     $('#employeeTable').on('click', '.delete-btn', removeEmployee);
 
     render();
@@ -14,57 +19,66 @@ function onReady() {
 
 function addEmployee() {
     console.log('in addEmployee');
-//reference the submitPost function in the event-state-render-starter
+    //add employee object
     let person = {
         firstName: $('#firstName').val(),
         lastName: $('#lastName').val(),
         id: $('#ID').val(),
         title: $('#title').val(),
         annualSalary: $('#annualSalary').val(),
-        
         arrayId: arrayId
     }
+    //increment our global arrayId
     arrayId++;
-
+    //clear inputs 
     $('#firstName').val('');
     $('#lastName').val('');
     $('#ID').val('');
     $('#title').val('');
     $('#annualSalary').val('');
-
+    //push entry to global employees array
     employees.push(person);
-
+    //render to DOM
     render();
 }
 
 function removeEmployee() {
-    
+    //initialize new array for employees not deleted
     let newEntries = [];
 
     console.log('in removeEmployee');
-//reference the onDelete function in the event-state-render-starter
-//use ID as your identifier
+
+    //initalize deleteThis with with bling this and the attribute 'id'
+    //of the removeEmployee button
     let deleteThis = $(this).attr('id')
 
 
     console.log('deleteThis:', deleteThis);
-
+    //loop through the employees array, if the id of the remove 
+    //button that we click does not match the person.arrayId
+    //we push those objects into the newEntries array
+    //and console log to check
 for (let person of employees) {
     if (person.arrayId != deleteThis) {
         newEntries.push(person);
         console.log(person.arrayId, deleteThis);
     }
 }
+//se the new value of the employees array to our newly created
+//newEntries array
 employees = newEntries;
 console.log(newEntries);
+//render
 render();
 }
 
 function render() {
-    $('#employeeTable').empty();
-
+    //empty the contents of the employeeTable
+    $('#tableBody').empty();
+    //loop through the employees array and append to the DOM
+    //the values of the object keys
     for (let person of employees) {
-        $('#employeeTable').append(`
+        $('#tableBody').append(`
         <tr>
             <td>${person.firstName}</td>
             <td>${person.lastName}</td>
@@ -76,10 +90,9 @@ function render() {
             <button id ="${person.arrayId}"class="delete-btn">
                 Remove Employee
             </button>
-        </td>
+            </td>
         </tr>
-        `)
+        `)// dynamically create a remove employee button with the 
+          //class delete-btn and an Id matching the object appended to the DOM
     }
-
-//reference render function in the event-state-render-starter
 }
